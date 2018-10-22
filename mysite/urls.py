4cb1781f8f74df6dec4f57198befba7595e170e9
younganakin <joenjuguna53@gmail.com>
2018-10-22 05:42:36 +0300

    Modify setting and urls.

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('ads/', include('ads.urls')),
     path('advertisement/', include('advertisement.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
